64a84870a3922090fe63dcda2d71083b6a407ce0
ram <rschoudhary1999@gmail.com>
2025-07-20 20:24:44 +0530

    feat: implement AllNetworksTable and NetworkDetailsPage components; refactor NetworksPage to use new components and improve loading state

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -30,7 +30,7 @@ export const DataTable = <TData, TValue>({
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -10 }}
       transition={{ duration: 0.3 }}
-      className="rounded-md border border-neutral-200 dark:border-neutral-800"
+      className="rounded-md border overflow-hidden border-neutral-200 dark:border-neutral-800"
     >
       <div className={cn(["[&>div]:max-h-96", className])}>
         <Table className="[&_td]:border-neutral-200 [&_th]:border-neutral-200 dark:[&_td]:border-neutral-800 dark:[&_th]:border-neutral-800 border-separate border-spacing-0 [&_tfoot_td]:border-t [&_th]:border-b [&_tr]:border-none [&_tr:not(:last-child)_td]:border-b">
--- src/pages/networks/networks.tsx
+++ src/pages/networks/networks.tsx
@@ -1,9 +1,19 @@
-import { NetworksTable } from "./features/network-table";
+import { Loader } from "@/components/ui/loader";
+import { Suspense } from "react";
+import { AllNetworksTable } from "./features/all-networks-table";
 
 export const NetworksPage = () => {
   return (
-    <div>
-      <NetworksTable />
+    <div className="p-2">
+      <Suspense
+        fallback={
+          <div className="w-fit h-fit z-100 absolute top-1/2 translate-y-[-50%] left-1/2 translate-x-[-50%] bg-transparent flex justify-center items-center">
+            <Loader />
+          </div>
+        }
+      >
+        <AllNetworksTable />
+      </Suspense>
     </div>
   );
 };
